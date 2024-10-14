2e5e20800537f1033c0737c644f2859bab3d2eb6
amkkr <55781271+amkkr@users.noreply.github.com>
2024-10-14 10:06:08 +0900

    update: madbd1 layout

--- keyboards/madbd1/keymaps/default/keymap.c
+++ keyboards/madbd1/keymaps/default/keymap.c
@@ -26,14 +26,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_ENT,
         KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPC,     KC_GRV,  KC_QUOT, KC_COMM, KC_DOT,  KC_SLSH,
-                                KC_LGUI,     KC_TRNS,    KC_ESC,  KC_RALT
+                                    KC_LGUI, KC_TRNS,    LT(3, KC_ESC),  KC_RALT
     ),
 
     [2] = LAYOUT(
         KC_TAB,  KC_NO, KC_NO, KC_NO, KC_END,  KC_NO,      KC_HOME, KC_NO,    KC_NO, KC_NO,   KC_NO, KC_DEL,
         KC_LCTL, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_NO,      KC_LEFT, KC_DOWN,  KC_UP, KC_RGHT, KC_NO, KC_ENT,
         KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO,   KC_PGUP,    KC_NO,   KC_NO,    KC_NO, KC_NO,   KC_NO,
-                                KC_LGUI,       KC_TRNS,    KC_TRNS,  KC_RALT
-    )
+                                KC_LCTL,       LT(3, KC_ESC),      KC_TRNS,  KC_RALT
+    ),
 
+    [3] = LAYOUT(
+        KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+        KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,  KC_NO,
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,
+                                 KC_LGUI, LT(1,KC_SPC),    LT(2, KC_ESC), KC_RALT
+    )
 };
