66811c47ed89d6ce229b23e003f91e14d634d57d
amkkr <55781271+amkkr@users.noreply.github.com>
2024-12-28 12:01:55 +0900

    update: print-screen

--- keyboards/madbd34/keymaps/default/keymap.c
+++ keyboards/madbd34/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         KC_TAB,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                             KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_ENT,
-        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPC,                            KC_GRV,  KC_QUOT, KC_COMM, KC_DOT,  KC_SLSH,
+        KC_LSFT, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_SPC,                            KC_GRV,  KC_QUOT, KC_COMM, KC_DOT,  KC_SLSH,
                         KC_LCTL, KC_LGUI, LT(1,KC_SPC),           LT(3, KC_ESC), KC_RALT,KC_NO
     ),
 
