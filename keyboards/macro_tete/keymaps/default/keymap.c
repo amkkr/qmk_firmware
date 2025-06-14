8c303ede283c656dd257142e74a1e69224d9394c
amkkr <55781271+amkkr@users.noreply.github.com>
2025-06-14 18:45:55 +0900

    feat: windows向けに変更

--- keyboards/macro_tete/keymaps/default/keymap.c
+++ keyboards/macro_tete/keymaps/default/keymap.c
@@ -4,8 +4,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // [0] = LAYOUT(
+        // LGUI(KC_A),LGUI(KC_Y),LGUI(KC_Z),
+        // LGUI(KC_V),LGUI(KC_C),LGUI(KC_X)
+    // )
     [0] = LAYOUT(
-        LGUI(KC_A),LGUI(KC_Y),LGUI(KC_Z),
-        LGUI(KC_V),LGUI(KC_C),LGUI(KC_X)
+        LCTL(KC_Z),LCTL(KC_Y),LCTL(KC_A),
+        LCTL(KC_X),LCTL(KC_C),LCTL(KC_V)
     )
 };
